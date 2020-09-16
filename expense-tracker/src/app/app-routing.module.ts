e3eb84c90b3a655c3424c5292d6ac90b8cd0ccb0
AaskaS <aaska18@gmail.com>
2020-09-16 14:16:03 -0400

    added routing

--- expense-tracker/src/app/app-routing.module.ts
+++ expense-tracker/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddExpenseComponent } from './add-expense/add-expense.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full'},
+  { path: 'welcome', component: WelcomeComponent},
+  { path: 'add-expense', component: AddExpenseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- expense-tracker/src/app/app.module.ts
+++ expense-tracker/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddExpenseComponent } from './add-expense/add-expense.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddExpenseComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
