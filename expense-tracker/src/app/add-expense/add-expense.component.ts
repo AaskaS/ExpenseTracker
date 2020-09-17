3d8b42669a7cdd12659ff5e93aa85261cec0b1ab
AaskaS <aaska18@gmail.com>
2020-09-17 17:04:28 -0400

    simple adding to and deleting from expense table functionality

--- expense-tracker/src/app/add-expense/add-expense.component.ts
+++ expense-tracker/src/app/add-expense/add-expense.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-add-expense',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-expense.component.css']
 })
 export class AddExpenseComponent implements OnInit {
+  expenses:String[] = [];
+  rolelist = [];
+  constructor(private router: Router) { }
 
-  constructor() { }
+submit(){
+  
+    var expense = 
+    {
+        "date": (<HTMLInputElement>document.getElementById("date")).value,
+        "place": (<HTMLInputElement>document.getElementById("place")).value,
+        "cost":(<HTMLInputElement>document.getElementById("cost")).value,
+        "category":(<HTMLInputElement>document.getElementById("category")).value,
+    };
+    this.rolelist.push(expense);
+    console.log(this.rolelist);
+
+    for(var i = 0; i < this.rolelist.length; i ++){
+      console.log(this.rolelist[i].date);
+    }
+
+  }
+
+delete(value:number){
+  console.log(value);
+  this.rolelist.splice(value, 1);
+}
 
   ngOnInit() {
   }
 
+  
 }
