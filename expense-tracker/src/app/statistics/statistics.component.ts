79e45ded04d068b3aa9c99281a84613bea5bbd4c
AaskaS <aaska18@gmail.com>
2020-09-22 13:15:58 -0400

    added a pie chart with mock data

--- expense-tracker/src/app/statistics/statistics.component.ts
+++ expense-tracker/src/app/statistics/statistics.component.ts
@@ -7,52 +7,61 @@ import { getInstanceByDom, connect } from 'echarts';
   styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent implements OnInit {
-  options = {
-    color: ['#3398DB'],
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'shadow',
-      },
+  public options = {
+    tooltip : {
+      trigger: 'item',
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
     },
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true,
+    legend: {
+      orient: 'vertical',
+      left: 'left',
+      data: ['Food', 'Grocery', 'Health', 'Household', 'Transportation', 'Personal', 'Electronics', 'Misc']
     },
-    xAxis: [
+    series : [
       {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-        axisTick: {
-          alignWithLabel: true,
-        },
-      },
-    ],
-    yAxis: [
-      {
-        type: 'value',
-      },
-    ],
-    series: [
-      {
-        name: 'Counters',
-        type: 'bar',
-        barWidth: '60%',
-        data: [10, 52, 200, 334, 390, 330, 220],
-      },
-    ],
+        name: 'Number of Requests by Request Type',
+        type: 'pie',
+        radius : '75%',
+        center: ['50%', '60%'],
+        data:[
+          {value:335, name:'Food'},
+          {value:310, name:'Grocery'},
+          {value:234, name:'Health'},
+          {value:135, name:'Household'},
+          {value:1548, name:'Transportation'},
+          {value:456, name:'Personal'},
+          {value:520, name:'Electronics'},
+          {value:670, name:'Misc'}
+        ],
+        itemStyle: {
+          emphasis: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
   };
+
   constructor() {}
+  ngOnInit(){
+
+  }
 
   ngAfterViewInit() {
     setTimeout(() => {
       const chartElement1 = document.getElementById('chart1');
-      const chartElement2 = document.getElementById('chart2');
+      //const chartElement2 = document.getElementById('chart2');
       const chart1 = getInstanceByDom(chartElement1);
-      const chart2 = getInstanceByDom(chartElement2);
-      connect([chart1, chart2]);
+      //const chart2 = getInstanceByDom(chartElement2);
+      //connect([chart1, chart2]);
     });
   }
 }
+
+/*
+
+data: [10, 52, 200, 334, 390, 330, 220, 150],
+
+*/
